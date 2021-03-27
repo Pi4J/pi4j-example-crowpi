b16ba66b892754da04bf47d14dd85533ae69cb18
Tobias Siegrist <tobias.siegrist@students.fhnw.ch>
2021-03-27 16:05:32 +0100

    demo code with some reading of brightness and work with the value

--- src/main/java/ch/fhnw/crowpi/applications/LightSensorApp.java
+++ src/main/java/ch/fhnw/crowpi/applications/LightSensorApp.java
@@ -4,14 +4,41 @@ import ch.fhnw.crowpi.Application;
 import ch.fhnw.crowpi.components.LightSensorComponent;
 import com.pi4j.context.Context;
 
-import static java.lang.Thread.sleep;
-
 public class LightSensorApp implements Application {
 
     @Override
     public void execute(Context pi4j) {
-        final var sensor = new LightSensorComponent(pi4j) ;
+        // Initialize a new Light Sensor
+        final var sensor = new LightSensorComponent(pi4j);
 
+        // Define a measurement interval in millis (to short interval can't be handled by the sensor)
+        final int DELAY = 1000;
+        // Define a number of loops until the App shuts down
+        final int NUMBER_OF_LOOPS = 20;
+        // Define some light limits to work with
+        final double DARK_VALUE = 150;
+        final double BRIGHT_VALUE = 3000;
 
+        // Loop until number of loops is reached
+        for (int i = 0; i < NUMBER_OF_LOOPS; i++) {
+            // Loop a measurement until a button is pressed
+            double value = sensor.readLight(2);
+
+            // Work with the measure value to define some actions
+            if (value < DARK_VALUE) {
+                System.out.println("Whoo that's dark... You should turn on light");
+            } else if (value >= DARK_VALUE && value < BRIGHT_VALUE) {
+                System.out.println("I feel good. Very nice light in here!");
+            } else if (value >= BRIGHT_VALUE) {
+                System.out.println("Oh no .. it's so bright ... please ... please turn off the light");
+            }
+
+            // Delay the thread
+            try {
+                Thread.sleep(DELAY);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 }
