f92aff0e79ac450ee0dac4925d6323abe30888cb
Tobias Siegrist <tobias.siegrist@students.fhnw.ch>
2021-03-30 18:01:51 +0200

    on work...

--- src/main/java/ch/fhnw/crowpi/applications/TouchSensorApp.java
+++ src/main/java/ch/fhnw/crowpi/applications/TouchSensorApp.java
@@ -3,6 +3,8 @@ package ch.fhnw.crowpi.applications;
 import ch.fhnw.crowpi.Application;
 import ch.fhnw.crowpi.components.TouchSensorComponent;
 import com.pi4j.context.Context;
+import com.pi4j.io.gpio.digital.DigitalStateChangeEvent;
+import com.pi4j.io.gpio.digital.DigitalStateChangeListener;
 
 import static java.lang.Thread.sleep;
 
@@ -13,9 +15,11 @@ public class TouchSensorApp implements Application {
 
         final var touchSensor = new TouchSensorComponent(pi4j);
 
+        touchSensor.addListener(this::onTouched);
+
         for (int i = 0; i < 10; i++) {
 
-            System.out.println("State: " + touchSensor.testReadState());
+            System.out.println("State: " + touchSensor.isTouched());
 
             try {
                 sleep(1000);
@@ -23,5 +27,9 @@ public class TouchSensorApp implements Application {
                 e.printStackTrace();
             }
         }
+    }
+
+    public void onTouched() {
+        System.out.println("HO HO HO");
     }
 }
--- src/main/java/ch/fhnw/crowpi/components/TouchSensorComponent.java
+++ src/main/java/ch/fhnw/crowpi/components/TouchSensorComponent.java
@@ -1,42 +1,57 @@
 package ch.fhnw.crowpi.components;
 
 import com.pi4j.context.Context;
-import com.pi4j.io.gpio.digital.DigitalInput;
-import com.pi4j.io.gpio.digital.DigitalInputConfig;
-import com.pi4j.io.gpio.digital.DigitalState;
-import com.pi4j.io.gpio.digital.PullResistance;
+import com.pi4j.event.Listener;
+import com.pi4j.io.gpio.digital.*;
 
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Callable;
+import java.util.function.Consumer;
+
+@FunctionalInterface
+public interface DigitalEvent {
+    void onTouch();
+}
+
 
 public class TouchSensorComponent {
     protected final DigitalInput din;
     protected static final int DEFAULT_PIN = 17;
-    protected static final PullResistance DEFAULT_PULL_RESISTANCE = PullResistance.PULL_UP;
 
     public TouchSensorComponent(Context pi4j) {
-        this(pi4j, DEFAULT_PIN, DEFAULT_PULL_RESISTANCE);
+        this(pi4j, DEFAULT_PIN);
     }
 
-    public TouchSensorComponent(Context pi4j, int address, PullResistance pullResistance) {
-        this.din = pi4j.create(buildDinConfig(pi4j, address, pullResistance));
+    public TouchSensorComponent(Context pi4j, int address) {
+        this.din = pi4j.create(buildDigitalInputConfig(pi4j, address));
     }
 
-    public DigitalState testReadState() {
-        System.out.println(din.toString());
-        System.out.println(din.provider().toString());
+    public boolean isTouched() {
+        return din.state().isHigh();
+    }
+
+    public DigitalState getState() {
         return din.state();
+    }
+
+    public void addListener(Callable<boolean> onTouched) {
+        din.addListener(event -> { onTouched(); });
+    }
+
+    public void removeListener(DigitalStateChangeListener listener) {
+        din.removeListener(listener);
     }
 
     protected DigitalInput getDigitalInput() {
         return din;
     }
 
-    protected DigitalInputConfig buildDinConfig(Context pi4j, int address, PullResistance pullResistance) {
+    protected DigitalInputConfig buildDigitalInputConfig(Context pi4j, int address) {
         return DigitalInput.newConfigBuilder(pi4j)
                 .id("BCM" + address)
                 .name("TouchSensor")
                 .address(address)
-                .pull(pullResistance)
+                .pull(PullResistance.PULL_UP)
                 .build();
     }
+
 }
