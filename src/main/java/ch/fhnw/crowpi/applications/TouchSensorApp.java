02c5def6ee037977fb769674744220518c2d50e7
Tobias Siegrist <tobias.siegrist@students.fhnw.ch>
2021-03-31 00:36:33 +0200

    commented demo applicaton touch sensor

--- src/main/java/ch/fhnw/crowpi/applications/TouchSensorApp.java
+++ src/main/java/ch/fhnw/crowpi/applications/TouchSensorApp.java
@@ -9,14 +9,17 @@ import static java.lang.Thread.sleep;
 public class TouchSensorApp implements Application {
     @Override
     public void execute(Context pi4j) {
-        System.out.println("Touch Sensor is now activated.");
-
+        // create a touch sensor instance
         final var touchSensor = new TouchSensorComponent(pi4j);
 
+        // create a listener to handle touch events
         Object listenerObject = touchSensor.addListener(state -> {
             System.out.println("State Changed! New State: " + state);
         });
 
+        System.out.println("Touch Sensor is now activated.");
+
+        // just any delay
         for (int i = 20; i > 0; i--) {
             System.out.println("Time until Eventlistener is killed: " + i + " seconds...");
 
@@ -26,12 +29,14 @@ public class TouchSensorApp implements Application {
             }
         }
 
+        // remove the created listener because we do not need it anymore
         touchSensor.removeListener(listenerObject);
 
+        // end the program as soon as isTouched returns true
         System.out.println("Press again to end this application");
-
         while (!touchSensor.isTouched()) {
             try {
+                //noinspection BusyWait
                 sleep(10);
             } catch (InterruptedException ignored) {
             }
