44d6052447ca2640f4a3b854fbfc291d3fe2ac89
Pascal Mathis <mail@pascalmathis.com>
2021-04-09 01:41:11 +0200

    feat: generic event handling interface

---
+++ src/main/java/ch/fhnw/crowpi/components/events/EventHandler.java
@@ -0,0 +1,17 @@
+package ch.fhnw.crowpi.components.events;
+
+/**
+ * Generic functional interface for event handlers called by component event listeners.
+ *
+ * @param <V> Type of value which gets passed once an event occurs.
+ */
+@FunctionalInterface
+public interface EventHandler<V> {
+    /**
+     * Handles a specific event based on implementation needs.
+     *
+     * @param listener Event listener which triggered this handler
+     * @param value    Event value
+     */
+    void handle(EventListener listener, V value);
+}
