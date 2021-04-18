533d8566da03069a00435e3afa1d67405e44761c
Tobias Siegrist <tobias.siegrist@students.fhnw.ch>
2021-04-18 17:55:43 +0200

    feat: line writing possible now

--- src/main/java/ch/fhnw/crowpi/components/LcdDisplayComponent.java
+++ src/main/java/ch/fhnw/crowpi/components/LcdDisplayComponent.java
@@ -1,14 +1,7 @@
 package ch.fhnw.crowpi.components;
 
-import ch.fhnw.crowpi.components.events.DigitalEventProvider;
 import ch.fhnw.crowpi.components.internal.MCP23008;
 import com.pi4j.context.Context;
-import com.pi4j.io.gpio.digital.DigitalInput;
-import com.pi4j.io.gpio.digital.DigitalInputConfig;
-import com.pi4j.io.gpio.digital.DigitalState;
-import com.pi4j.io.gpio.digital.PullResistance;
-import com.pi4j.io.i2c.I2C;
-import com.pi4j.io.i2c.I2CConfig;
 
 // Diese Ding benutzt IO via MCP23008.
 // Also I2C -> MCP23008 -> IO -> Display
@@ -16,8 +9,10 @@ import com.pi4j.io.i2c.I2CConfig;
 // braucht komponente die richtig auf MCP schreibt
 // helligkeit nur mit poti
 public class LcdDisplayComponent {
+    /**
+     * IO Component used to Display
+     */
     private final MCP23008 mcp;
-
     /**
      * Those default address are to use this class with default CrowPi setup
      */
@@ -25,7 +20,7 @@ public class LcdDisplayComponent {
     private static final int DEFAULT_DEVICE = 0x21;
 
     /**
-     * Creates a new light sensor component using the default setup.
+     * Creates a new LCD Display component using the default setup.
      *
      * @param pi4j Pi4J context
      */
@@ -33,53 +28,154 @@ public class LcdDisplayComponent {
         this(pi4j, DEFAULT_BUS, DEFAULT_DEVICE);
     }
 
+    /**
+     * Creates a new  LCD Display component with custom bus, device address
+     *
+     * @param pi4j   Pi4J context
+     * @param bus    Custom I2C bus address
+     * @param device Custom device address on I2C
+     */
     public LcdDisplayComponent(Context pi4j, int bus, int device) {
         this.mcp = new MCP23008(pi4j, bus, device);
         this.mcp.initializeIo(MCP_IO_CONFIG);
-        this.initializeLcd();
     }
 
+    // FIXME IM JUST A TEST METHOD!
     public void play() {
-        write8('A', true);
+        write('A', true);
     }
 
-    protected void initializeLcd() {
+    /**
+     * Initializes the LCD Display
+     */
+    public void initialize() {
         // Enable backlight
-        mcp.setAndWritePin(LCD_LIGHT, true);
+        setDisplayBacklight(true);
 
         // Initialize display
-        write8((byte) 0b001_10011);
-        write8((byte) 0b001_10010);
+        write((byte) 0b001_10011);
+        write((byte) 0b001_10010);
 
         // Initialize display settings
-        byte displayControl = (byte) (LCD_DISPLAYON | LCD_CURSORON | LCD_BLINKOFF);
+        byte displayControl = (byte) (LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF);
         byte displayFunction = (byte) (LCD_4BITMODE | LCD_1LINE | LCD_2LINE | LCD_5x8DOTS);
         byte displayMode = (byte) (LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT);
 
-
-        execute(LCD_DISPLAYCONTROL, displayControl);
-        // write8((byte) (LCD_DISPLAYCONTROL | displayControl));
-        write8((byte) (LCD_FUNCTIONSET | displayFunction));
-        write8((byte) (LCD_ENTRYMODESET | displayMode));
+        // Write Display settings
+        executeCommand(LCD_DISPLAYCONTROL, displayControl);
+        write((byte) (LCD_FUNCTIONSET | displayFunction));
+        write((byte) (LCD_ENTRYMODESET | displayMode));
 
         // Clear display
-        write8(LCD_CLEARDISPLAY);
+        clearDisplay();
+    }
+
+    /**
+     * Write a Line of Text on the LCD Display
+     *
+     * @param text Text to display
+     * @param line Select Line of Display
+     */
+    public void writeLine(String text, int line) {
+        returnHome();
+        clearLine(line);
+        setCursorToLine(line);
+
+        for (int i = 0; i < text.length(); i++) {
+            write(text.charAt(i), true);
+        }
+    }
+
+    /**
+     * Write a text upto 32 characters to the display
+     *
+     * @param text Text to write to the display
+     */
+    public void writeText(String text) {
+        setCursorToLine(1);
+
+        for (int i = 0; i < text.length(); i++) {
+            write(text.charAt(i), true);
+
+            if (i == 15) {
+                setCursorToLine(2);
+            }
+        }
+    }
+
+    public void setCursorToLine(int number) {
+        if (number > 2 || number < 1) {
+            throw new IllegalArgumentException("CrowPi Display has only 2 Rows!");
+        }
+
+        executeCommand(LCD_SETDDRAMADDR, LCD_ROW_OFFSETS[number - 1]);
+    }
+
+    /**
+     * Enable and Disable the Backlight of the LCD Display
+     *
+     * @param state Set Backlight ON or OFF
+     */
+    public void setDisplayBacklight(boolean state) {
+        mcp.setAndWritePin(LCD_LIGHT, state);
+    }
+
+    /**
+     * Clears the display and return the cursor to home
+     */
+    public void clearDisplay() {
+        write(LCD_CLEARDISPLAY);
         sleep(3);
+        returnHome();
+    }
 
-        // Move the cursor to its home position
-        write8(LCD_RETURNHOME);
+    /**
+     * Clears a line of the display
+     *
+     * @param line Select line to clear
+     */
+    public void clearLine(int line) {
+        setCursorToLine(line);
+
+        for (int i = 0; i < 16; i++) {
+            write(' ', true);
+        }
+    }
+
+    /**
+     * Returns the Cursor to Home Position (First line, first character)
+     */
+    public void returnHome() {
+        write(LCD_RETURNHOME);
         sleep(3);
     }
 
-    public void execute(byte command, byte data) {
-        write8((byte) (command | data));
+    /**
+     * Execute Display commands
+     *
+     * @param command Select the LCD Command
+     * @param data    Setup command data
+     */
+    protected void executeCommand(byte command, byte data) {
+        write((byte) (command | data));
     }
 
-    public void write8(int c) {
-        write8(c, false);
+    /**
+     * Write a number (byte) to the LCD Display
+     *
+     * @param c Number to write to the Display
+     */
+    protected void write(int c) {
+        write(c, false);
     }
 
-    public void write8(int b, boolean charMode) {
+    /**
+     * Write a Number (byte) or character according to the LCD Display
+     *
+     * @param b        Data to write to the display
+     * @param charMode Select data is a number or character
+     */
+    protected void write(int b, boolean charMode) {
         b &= 0xFF;
         mcp.setAndWritePin(LCD_RS, charMode);
 
@@ -100,20 +196,19 @@ public class LcdDisplayComponent {
         mcp.pulsePin(LCD_EN, DEFAULT_PULSE_WIDTH);
     }
 
-    public void message(String s) {
-        for (int i = 0; i < s.length(); i++) {
-            write8(s.charAt(i), true);
-        }
-    }
 
-    private void sleep(int millis) {
+    /**
+     * FIXME with component sleep!
+     *
+     * @param millis
+     */
+    public void sleep(int millis) {
         try {
             Thread.sleep(millis);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
     }
-
 
     // Default MCP Configuration
     byte MCP_IO_CONFIG = 0x00;
@@ -129,7 +224,7 @@ public class LcdDisplayComponent {
     byte LCD_CURSORSHIFT = 0x10;
     byte LCD_FUNCTIONSET = 0x20;
     byte LCD_SETCGRAMADDR = 0x40;
-    //byte LCD_SETDDRAMADDR = 0x80;
+    byte LCD_SETDDRAMADDR = (byte) 0x80;
 
 
     // Entry flags
@@ -140,36 +235,36 @@ public class LcdDisplayComponent {
 
 
     // Control flags
-    byte LCD_DISPLAYON           =0x04;
-    byte LCD_DISPLAYOFF          =0x00;
-    byte LCD_CURSORON            =0x02;
-    byte LCD_CURSOROFF           =0x00;
-    byte LCD_BLINKON             =0x01;
-    byte LCD_BLINKOFF            =0x00;
+    byte LCD_DISPLAYON = 0x04;
+    byte LCD_DISPLAYOFF = 0x00;
+    byte LCD_CURSORON = 0x02;
+    byte LCD_CURSOROFF = 0x00;
+    byte LCD_BLINKON = 0x01;
+    byte LCD_BLINKOFF = 0x00;
 
     // Move flags
-    byte LCD_DISPLAYMOVE         =0x08;
-    byte LCD_CURSORMOVE          =0x00;
-    byte LCD_MOVERIGHT           =0x04;
-    byte LCD_MOVELEFT            =0x00;
+    byte LCD_DISPLAYMOVE = 0x08;
+    byte LCD_CURSORMOVE = 0x00;
+    byte LCD_MOVERIGHT = 0x04;
+    byte LCD_MOVELEFT = 0x00;
 
     // Function set flags
-    byte LCD_8BITMODE           = 0x10;
-    byte LCD_4BITMODE            =0x00;
-    byte LCD_2LINE               =0x08;
-    byte LCD_1LINE               =0x00;
-    byte LCD_5x10DOTS            =0x04;
-    byte LCD_5x8DOTS             =0x00;
+    byte LCD_8BITMODE = 0x10;
+    byte LCD_4BITMODE = 0x00;
+    byte LCD_2LINE = 0x08;
+    byte LCD_1LINE = 0x00;
+    byte LCD_5x10DOTS = 0x04;
+    byte LCD_5x8DOTS = 0x00;
 
     // Offset forup to 4 rows.
-    byte[] LCD_ROW_OFFSETS         ={0x00,0x40,0x14,0x54};
+    byte[] LCD_ROW_OFFSETS = {0x00, 0x40, 0x14, 0x54};
 
     // Char LCDplate buttonnames.
-   int SELECT                  =0;
-   int RIGHT                   =1;
-   int DOWN                    =2;
-   int UP                      =3;
-   int LEFT                    =4;
+    int SELECT = 0;
+    int RIGHT = 1;
+    int DOWN = 2;
+    int UP = 3;
+    int LEFT = 4;
 
     /**
      * Pin out LCD auf MCP
