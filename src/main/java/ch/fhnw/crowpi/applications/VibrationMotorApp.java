91920ea73d81bad11bfb29f0ea95977524fd9198
Tobias Siegrist <tobias.siegrist@students.fhnw.ch>
2021-04-23 17:44:25 +0200

    feat: example and testing done

--- src/main/java/ch/fhnw/crowpi/applications/VibrationMotorApp.java
+++ src/main/java/ch/fhnw/crowpi/applications/VibrationMotorApp.java
@@ -4,26 +4,44 @@ import ch.fhnw.crowpi.Application;
 import ch.fhnw.crowpi.components.VibrationMotorComponent;
 import com.pi4j.context.Context;
 
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * This example shows how to use the vibration motor. As this component is a simple digital output it is really easy
+ * to use. An absolute beginner friendly component of the CrowPi
+ */
 public class VibrationMotorApp implements Application {
     @Override
     public void execute(Context pi4j) {
         VibrationMotorComponent vibrationMotor = new VibrationMotorComponent(pi4j);
 
+        // Short test if the component is working
         vibrationMotor.on();
+        sleep(500);
+        vibrationMotor.off();
 
-        for (int j = 0; j < 2; j++) {
+        // Write to ask the use something
+        System.out.println("Are you awake? Answer with YES if you are...");
+        // Scanner is used to read the input from the commandline
+        Scanner scanner = new Scanner(System.in);
 
-            for (int i = 1; i < 75; i = i + 2) {
+        // Loops until YES is found
+        while (!scanner.nextLine().equalsIgnoreCase("YES")) {
+            // Do some pulses to create a strange noise
+            for (int i = 10; i < 100; i++) {
+                // As you see use pulse is a lot more comfortable than using .on -> sleep -> off
                 vibrationMotor.pulse(i);
                 sleep(i);
             }
-
-            for (int i = 75; i > 1; i = i - 2) {
+            for (int i = 100; i > 10; i--) {
                 vibrationMotor.pulse(i);
                 sleep(i);
             }
+            System.out.println("Are you awake? Answer with YES if you are...");
         }
+
+        // Awake so we can turn of the vibration motor now.
+        vibrationMotor.off();
     }
 }
