2031df869c0a2da9870ddbb42f7c63a675c68c80
Tobias Siegrist <tobias.siegrist@students.fhnw.ch>
2021-05-20 16:08:35 +0200

    feat: first component draft

--- src/main/java/com/pi4j/crowpi/components/SoundSensorComponent.java
+++ src/main/java/com/pi4j/crowpi/components/SoundSensorComponent.java
@@ -10,7 +10,7 @@ import com.pi4j.io.gpio.digital.DigitalState;
 import com.pi4j.io.gpio.digital.PullResistance;
 
 /**
- * Implementation of the CrowPi sound sensor using GPIO with Pi4J
+ * Implementation of the CrowPi touch sensor using GPIO with Pi4J
  */
 public class SoundSensorComponent extends Component implements DigitalEventProvider<SoundSensorComponent.SoundState> {
     /**
@@ -18,13 +18,9 @@ public class SoundSensorComponent extends Component implements DigitalEventProvi
      */
     protected final DigitalInput digitalInput;
     /**
-     * Default Pin of sound sensor
+     * Default Pin of touch sensor
      */
     protected static final int DEFAULT_PIN = 24;
-    /**
-     * Debounce of input in microseconds
-     */
-    protected static final long DEFAULT_DEBOUNCE = 1000;
 
     /**
      * Handler for simple event when a noise is recognized
@@ -36,29 +32,28 @@ public class SoundSensorComponent extends Component implements DigitalEventProvi
     private SimpleEventHandler onSilence;
 
     /**
-     * Creates a new sound sensor component using the default setup.
+     * Creates a new touch sensor component using the default setup.
      *
      * @param pi4j Pi4J context
      */
     public SoundSensorComponent(Context pi4j) {
-        this(pi4j, DEFAULT_PIN, DEFAULT_DEBOUNCE);
+        this(pi4j, DEFAULT_PIN);
     }
 
     /**
-     * Create sound sensor component with custom parameters
+     * Create touch sensor component with custom parameters
      *
-     * @param pi4j     Pi4J context
-     * @param address  GPIO Pin of Raspberry
-     * @param debounce Time to debounce the sound sensor in microseconds
+     * @param pi4j    Pi4J context
+     * @param address GPIO Pin of Raspberry
      */
-    public SoundSensorComponent(Context pi4j, int address, long debounce) {
-        this.digitalInput = pi4j.create(buildDigitalInputConfig(pi4j, address, debounce));
+    public SoundSensorComponent(Context pi4j, int address) {
+        this.digitalInput = pi4j.create(buildDigitalInputConfig(pi4j, address));
         this.addListener(this::dispatchSimpleEvents);
     }
 
     /**
-     * Sets or disables the handler for the onNoise event.
-     * This event gets triggered whenever the sensor registers noise.
+     * Sets or disables the handler for the onTouch event.
+     * This event gets triggered whenever the sensor is touched.
      * Only a single event handler can be registered at once.
      *
      * @param handler Event handler to call or null to disable
@@ -68,35 +63,8 @@ public class SoundSensorComponent extends Component implements DigitalEventProvi
     }
 
     /**
-     * Reads the current sensor state
-     *
-     * @return Returns true if there is a noise currently registered by the sensor
-     */
-    public boolean isNoisy() {
-        return digitalInput.isLow();
-    }
-
-    /**
-     * Reads the current sensor state
-     *
-     * @return Returns true if it is silent around the sensor
-     */
-    public boolean isSilent() {
-        return digitalInput.isHigh();
-    }
-
-    /**
-     * Returns the SoundState of the sensor
-     *
-     * @return Returns the well-known SoundState according to the current noise level
-     */
-    public SoundState getState() {
-        return mapDigitalState(digitalInput.state());
-    }
-
-    /**
-     * Sets or disables the handler for the onSilence event.
-     * This event gets triggered whenever the sensor is no longer under noise
+     * Sets or disables the handler for the onRelease event.
+     * This event gets triggered whenever the sensor is no longer touched.
      * Only a single event handler can be registered at once.
      *
      * @param handler Event handler to call or null to disable
@@ -105,20 +73,11 @@ public class SoundSensorComponent extends Component implements DigitalEventProvi
         this.onSilence = handler;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public DigitalInput getDigitalInput() {
         return digitalInput;
     }
 
-    /**
-     * Maps a {@link DigitalState} to a well-known {@link SoundState}
-     *
-     * @param digitalState Pi4J digital state to map
-     * @return Mapped sound state
-     */
     @Override
     public SoundState mapDigitalState(DigitalState digitalState) {
         switch (digitalState) {
@@ -151,21 +110,20 @@ public class SoundSensorComponent extends Component implements DigitalEventProvi
      * Configure Digital Input
      *
      * @param pi4j    PI4J Context
-     * @param address GPIO Address of sound sensor
+     * @param address GPIO Address of touch sensor
      * @return Return Digital Input configuration
      */
-    protected DigitalInputConfig buildDigitalInputConfig(Context pi4j, int address, long debounce) {
+    protected DigitalInputConfig buildDigitalInputConfig(Context pi4j, int address) {
         return DigitalInput.newConfigBuilder(pi4j)
             .id("BCM" + address)
             .name("SoundSensor")
             .address(address)
             .pull(PullResistance.PULL_UP)
-            .debounce(debounce)
             .build();
     }
 
     /**
-     * All available states reported by the sound sensor component.
+     * All available states reported by the touch sensor component.
      */
     public enum SoundState {
         NOISE,
